9527ce341a33fbf69e09658015a76d55df5a6c34
vishal Joshi <vishal Joshi@VISHAL-IDEAPAD->
2023-01-27 14:22:11 +0530

    Add project files.

---
+++ Jokesweb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Jokesweb.Models;
+
+namespace Jokesweb.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Jokesweb.Models.Joke> Joke { get; set; }
+    }
+}
